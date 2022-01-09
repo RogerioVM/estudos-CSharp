1fdb9203a24b19e3e2c40f89474cc936ccd60471
Rogerio <rogerio1597@gmail.com>
2022-01-09 19:39:04 -0300

    Adicionando modulos de debug

--- Chapter06/Ch06Ex03/Ch06Ex03/Program.cs
+++ Chapter06/Ch06Ex03/Ch06Ex03/Program.cs
@@ -4,6 +4,16 @@ namespace Ch06Ex03
 {
     class Program
     {
+        static string myString = "A string global";
+
+        static void String()
+        {
+            string myString = "Local";
+            Program.myString = "Manipulação da string global";
+            Console.WriteLine($"String na função: {myString}");
+            Console.WriteLine(Program.myString);
+        }
+
         // Parametros por referencia
         static int ShowDouble(ref int val)
         {
@@ -33,9 +43,11 @@ namespace Ch06Ex03
             //O valor referenciado pela função é mantido depois que passamos com a palavra "ref" no restante da aplicação: 
             Console.WriteLine($"Valor depois de chamado na função, sendo referenciado: { myValue}");
 
-
+            myString = "String global";
             double resultado = Numbers(9.5,8.3,7.9);
             Console.WriteLine($"Params argumentos: {resultado.ToString("F2")}");
+            String();
+            Console.WriteLine($"String na função Main(): {myString}");
             Console.ReadKey();
         }
     }
