3c8668eddea758975fbf25d4d9f4ae8ed9d7fcc0
Rogerio <rogerio1597@gmail.com>
2022-08-02 23:07:14 -0300

    Subindo projeto 11

---
+++ Chapter09/ClassLibraryTrain/ClassLibraryTrain/MyTrain.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibraryTrain
+{
+    public class Class1
+    {
+    }
+}
--- Chapter10/Ch10Ex02/Ch10Ex02/Program.cs
+++ Chapter10/Ch10Ex02/Ch10Ex02/Program.cs
@@ -21,7 +21,30 @@
             ClassA.ClassB myOtherObject = new ClassA.ClassB();
             myOtherObject.SetPrivateState(myObject, 999);
             Console.WriteLine($"myObject.State = {myObject.State}");
+
+            Profile profile = new Profile();
+            profile.Mostrar();
             Console.ReadKey();
+        }
+    }
+
+    class Profile
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int Picture { get; set; }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Digite o nome a ser inserido: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade a ser inserida: ");
+            int idade = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Seu nome é {nome}");
+            Console.WriteLine($"Sua idade é {idade}");
+
         }
     }
 }
